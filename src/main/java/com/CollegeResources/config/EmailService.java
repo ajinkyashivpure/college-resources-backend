9da5b0b1bf45a2a2cdb28ebbcc9c89939f208360
ajinkyashivpure <ajinkyashivpure@gmail.com>
2025-04-20 17:08:19 +0530

    application properties

--- src/main/java/com/CollegeResources/config/EmailService.java
+++ src/main/java/com/CollegeResources/config/EmailService.java
@@ -5,7 +5,6 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
-
 public class EmailService {
 
     private final JavaMailSender javaMailSender;
